efa24e20a1670ea9383bac28e10710bce21c0c40
Jakub Kottnauer <jk@jakubkottnauer.com>
2016-05-02 11:48:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,60 @@
+#react-load-script
+This package simplifies loading of 3rd party scripts in your React applications.
+
+#Motivation
+There are situations when you need to use a 3rd party JS library in your React application (jQuery, D3.js for rendering charts, etc.) but you don't need it everywhere and/or you want to use it only in a response to users actions. In cases like this, preloading the whole library when application starts is an unnecessary and expensive operation which could possibly slow down your application.
+
+Using the `Script` component this package provides you with, you can easily load any 3rd party scripts your applications needs directly in a relevant component and show a placeholder while the script is loading (e.g. a loading animation). As soon as the script is fully loaded, a callback function you'll have passed to `Script` is called (see example below).
+
+#API
+The package exports a single component with the following props:
+
+## `onCreate`
+Called as soon as the script tag is created.
+
+## `onError` (required)
+Called in case of an error with the script.
+
+## `onLoad` (required)
+Called when the requested script is fully loaded.
+
+## `url` (required)
+URL pointing to the script you want to load.
+
+#Example
+You can use the following code to load jQuery in your app:
+
+```jsx
+import Script from 'react-load-script'
+
+...
+
+render() {
+  return (
+    <Script
+      url="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"
+      onCreate={this.handleScriptCreate.bind(this)}
+      onError={this.handleScriptError.bind(this)}
+      onLoad={this.handleScriptLoad.bind(this)}
+    />
+  )
+}
+
+...
+
+handleScriptCreate() {
+  this.setState({ scriptLoaded: false })
+}
+
+handleScriptError() {
+  this.setState({ scriptError: true })
+}
+
+handleScriptLoad() {
+  this.setState({ scriptLoaded: true })
+}
+
+```
+
+#License
+MIT 2016
---
+++ src/index.jsx
@@ -0,0 +1,120 @@
+import invariant from 'invariant';
+import React, { PropTypes as RPT } from 'react';
+
+export default class Script extends React.Component {
+
+  static propTypes = {
+    onCreate: RPT.func,
+    onError:  RPT.func.isRequired,
+    onLoad:   RPT.func.isRequired,
+    url:      RPT.string.isRequired
+  };
+
+  // A dictionary mapping script URLs to a dictionary mapping
+  // component key to component for all components that are waiting
+  // for the script to load.
+  static scriptObservers = {};
+
+  // A dictionary mapping script URL to a boolean value indicating if the script
+  // has already been loaded.
+  static loadedScripts = {};
+
+  // A dictionary mapping script URL to a boolean value indicating if the script
+  // has failed to load.
+  static erroredScripts = {};
+
+  // A counter used to generate a unique id for each component that uses
+  // ScriptLoaderMixin.
+  static idCount = 0;
+
+  constructor(props) {
+    super(props);
+    this.scriptLoaderId = `id${this.constructor.idCount++}`; // eslint-disable-line space-unary-ops
+  }
+
+  componentDidMount() {
+    const { url } = this.props;
+
+    if (this.constructor.loadedScripts[url]) {
+      this.runCallback('onLoad');
+      return;
+    }
+
+    if (this.constructor.erroredScripts[url]) {
+      this.runCallback('onError');
+      return;
+    }
+
+    // If the script is loading, add the component to the script's observers
+    // and return. Otherwise, initialize the script's observers with the component
+    // and start loading the script.
+    if (this.constructor.scriptObservers[url]) {
+      this.constructor.scriptObservers[url][this.scriptLoaderId] = this.runCallback.bind(this);
+      return;
+    }
+
+    this.constructor.scriptObservers[url] = { [this.scriptLoaderId]: this.runCallback.bind(this) };
+
+    this.createScript();
+  }
+
+  componentWillUnmount() {
+    const { url } = this.props;
+    const observers = this.constructor.scriptObservers[url];
+
+    // If the component is waiting for the script to load, remove the
+    // component from the script's observers before unmounting the component.
+    if (observers)
+      delete observers[this.scriptLoaderId];
+  }
+
+  createScript() {
+    const { url } = this.props;
+    const script = document.createElement('script');
+
+    this.runCallback('onCreate', false);
+
+    script.src = url;
+    script.async = 1;
+
+    const callObserverFuncAndRemoveObserver = (shouldRemoveObserver) => {
+      const observers = this.constructor.scriptObservers[url];
+      Object.keys(observers).forEach(key => {
+        if (shouldRemoveObserver(observers[key]))
+          delete this.constructor.scriptObservers[url][this.scriptLoaderId];
+      });
+    };
+    script.onload = () => {
+      this.constructor.loadedScripts[url] = true;
+      callObserverFuncAndRemoveObserver(observer => {
+        observer('onLoad');
+        return true;
+      });
+    };
+
+    script.onerror = () => {
+      this.constructor.erroredScripts[url] = true;
+      callObserverFuncAndRemoveObserver(observer => {
+        observer('onError');
+        return true;
+      });
+    };
+
+    document.body.appendChild(script);
+  }
+
+  runCallback(type, required = true) {
+    const callback = this.props[type];
+
+    invariant(
+      !required || typeof callback === 'function',
+      `Callback ${type} must be function, got "${typeof callback}" instead`
+    );
+
+    return callback && callback();
+  }
+
+  render() {
+    return null;
+  }
+}
